a1ce7ab1814900373a9ff4a55dd72962bc11e187
Quinn Gifford <155132289+quinngifford@users.noreply.github.com>
2026-08-02 12:08:31 -0700

    Update resume download link in experience component

--- components/experience.tsx
+++ components/experience.tsx
@@ -64,7 +64,7 @@ export function Experience() {
           subtitle=""
           action={
             <Button size="lg" asChild>
-              <a href="/quinnresume.pdf" download>
+              <a href="/cv 2025-6 (14).pdf" download>
                 <Download className="mr-2 h-5 w-5" />
                 Download Resume
               </a>
